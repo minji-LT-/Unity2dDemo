2093a7b10df8cc23621536dccd36869d4a170858
unknown <jijun7885417@126.com>
2022-02-22 20:46:42 +0800

    x

---
+++ Assets/Scripts/EnemiesController.cs
@@ -0,0 +1,113 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemiesController : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    
+    private Animator anim;
+    private bool faceLeft;
+    private bool faceTop;
+    public Transform leftPoint, rightPoint;
+    public Transform topPoint, bottomPoint;
+    public LayerMask ground;
+    public Collider2D collider;
+
+    public float speed = 1.5f;
+    public float jumpForce = 5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        collider = GetComponent<Collider2D>();
+        anim = GetComponent<Animator>();
+
+        faceLeft = true;//Random.value < 0.5;
+        faceTop = Random.value < 0.5;
+
+        if (gameObject.tag == "EnemyFrog")
+        {
+            transform.localScale = new Vector3(Mathf.Abs(transform.localScale.x) * (faceLeft ? 1 : -1), transform.localScale.y, transform.localScale.z);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SwitchAnim();
+    }
+    void Movement()
+    {
+        if (gameObject.tag == "EnemyFrog")
+        {
+            if (transform.position.x - 1.5 < leftPoint.position.x)
+            {
+                faceLeft = false;
+
+                transform.localScale = new Vector3(-Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
+            }
+            else if (transform.position.x + 1.5 > rightPoint.position.x)
+            {
+                faceLeft = true;
+                transform.localScale = new Vector3(Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
+            }
+
+            if (faceLeft)
+            {
+                
+                
+                if (collider.IsTouchingLayers(ground))
+                {
+                    anim.SetBool("jumping", true);
+                    rb.velocity = new Vector2(-speed, jumpForce);
+                }
+            }
+            else
+            {
+                if (collider.IsTouchingLayers(ground))
+                {
+                    anim.SetBool("jumping", true);
+                    rb.velocity = new Vector2(speed, jumpForce);
+                }
+                
+            }            
+            
+        }
+        else if (gameObject.tag == "EnemyEagle")
+        {
+            if (faceTop)
+            {
+                rb.velocity = new Vector2(rb.velocity.x, speed);
+            }
+            else
+            {
+                rb.velocity = new Vector2(rb.velocity.x, -speed);
+            }
+            if (transform.position.y < bottomPoint.position.y)
+            {
+                faceTop = true;
+            }
+            else if (transform.position.y > topPoint.position.y)
+            {
+                faceTop = false;
+            }
+        }
+    }
+    void SwitchAnim()
+    {
+        if (anim.GetBool("jumping"))
+        {
+            if (rb.velocity.y < 0.1f)
+            {
+                anim.SetBool("falling", true);
+                anim.SetBool("jumping", false);
+            }
+        }
+        if (collider.IsTouchingLayers(ground) && anim.GetBool("falling"))
+        {
+            anim.SetBool("falling", false);
+        }
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,12 +6,19 @@ public class PlayerController : MonoBehaviour
 {
     private Rigidbody2D rb;
     private Animator anim;
+    private int Cherry, Diamond;
+
+    private bool isHurt;
+
     public float speed;
     public float jumpforce;
     public LayerMask ground;
     public Collider2D collider;
 
-    public int Cherry;
+    public UnityEngine.UI.Text CherryNumberLabel;
+    public UnityEngine.UI.Text DiamondNumberLabel;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -25,7 +32,11 @@ public class PlayerController : MonoBehaviour
     }
     private void FixedUpdate()
     {
-        Movement();
+        
+        if (!isHurt)
+        {
+            Movement();
+        }
         SwitchAnim();
     }
     void Movement()
@@ -40,7 +51,7 @@ public class PlayerController : MonoBehaviour
             anim.SetFloat("running", Mathf.Abs(horizontalmove));
         }
         // ½ÇÉ«ÌøÔ¾
-        if (Input.GetButtonDown("Jump"))
+        if (Input.GetButtonDown("Jump") && collider.IsTouchingLayers(ground))
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpforce * Time.deltaTime);
             anim.SetBool("jumping", true);
@@ -51,6 +62,10 @@ public class PlayerController : MonoBehaviour
     void SwitchAnim()
     {
         anim.SetBool("idle", false);
+        if (rb.velocity.y < 0.1f && !collider.IsTouchingLayers(ground))
+        {
+            anim.SetBool("falling", true);
+        }
         if (anim.GetBool("jumping"))
         {
             if (rb.velocity.y < 0)
@@ -58,7 +73,19 @@ public class PlayerController : MonoBehaviour
                 anim.SetBool("jumping", false);
                 anim.SetBool("falling", true);
             }
-        } else if (collider.IsTouchingLayers(ground))
+        }else if (isHurt)
+        {
+            anim.SetBool("hurt", true);
+            anim.SetFloat("running", 0);
+            if (Mathf.Abs(rb.velocity.x) < 0.1)
+            {
+                isHurt = false;
+                anim.SetBool("hurt", false);
+                anim.SetBool("idle", true);
+            }
+            
+        } 
+        else if (collider.IsTouchingLayers(ground))
         {
             anim.SetBool("falling", false);
             anim.SetBool("idle", true);
@@ -67,10 +94,43 @@ public class PlayerController : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.tag == "Collection")
+        if (collision.gameObject.tag == "Collection")
         {
             Destroy(collision.gameObject);
             Cherry += 1;
+            CherryNumberLabel.text = Cherry.ToString();
+        } else if (collision.gameObject.tag == "Collection2")
+        {
+            Destroy(collision.gameObject);
+            ++Diamond;
+            DiamondNumberLabel.text = Diamond.ToString();
         }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+       
+        if (collision.gameObject.tag == "EnemyFrog" || collision.gameObject.tag == "EnemyEagle")
+        {
+            if (anim.GetBool("falling"))
+            {
+                Destroy(collision.gameObject);
+                rb.velocity = new Vector2(rb.velocity.x, jumpforce * Time.deltaTime);
+                anim.SetBool("jumping", true);
+            }
+            else
+            {
+                if (transform.position.x < collision.gameObject.transform.position.x)
+                {
+                    rb.velocity = new Vector2(-5, rb.velocity.y);
+                    isHurt = true;
+                }
+                else if (transform.position.x > collision.gameObject.transform.position.x)
+                {
+                    rb.velocity = new Vector2(5, rb.velocity.y);
+                    isHurt = true;
+                }
+            }
+        }
+        
     }
 }
